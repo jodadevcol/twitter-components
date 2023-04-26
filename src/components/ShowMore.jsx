4e1ace6b8df89b3e7ba56b3889fbf86dda74458c
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-04-27 08:33:21 +1000

    feat: :hammer: add twitter component of anchor

---
+++ src/components/ShowMore.jsx
@@ -0,0 +1,9 @@
+import './ShowMore.css'
+
+export function ShowMore () {
+  return (
+    <a className='tw-showMore' href=''>
+      <span className='tw-showMore-label'>Show more</span>
+    </a>
+  )
+}
