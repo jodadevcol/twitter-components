aa5379770e9b98c2c4c895d175425a16c19411a7
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-04-27 08:32:59 +1000

    feat: :hammer: add twitter component of titles in boxes

---
+++ src/components/BoxHeader.jsx
@@ -0,0 +1,9 @@
+import './BoxHeader.css'
+
+export function BoxHeader () {
+  return (
+    <header className='tw-boxHeader'>
+      <h2 className='tw-boxHeader-title'>Who to follow</h2>
+    </header>
+  )
+}
